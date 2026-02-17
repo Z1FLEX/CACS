dad6973182c582f17dc669f6f7566cc4a6e5d3ac
Z1FLEX <zougariakram4@gmail.com>
2026-02-17 16:36:18 +0100

    init

---
+++ CACS/src/main/java/com/hsware/cacs/CacsApplication.java
@@ -0,0 +1,13 @@
+package com.hsware.cacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacsApplication.class, args);
+    }
+
+}
